96ea7d6c68769d5acb96d5228d8cf5e74422a96c
soggylithe <nivek1892@gmail.com>
2023-04-10 13:01:59 -0500

    Initial commit

---
+++ src/TabletCore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+
+namespace Soggylithe_Tablet
+{
+    public class ModSystemTablets: ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+
+            api.Logger.Notification("Loaded tablets.");
+
+            //TODO: register hotkey to swap offand with neck slot
+        }
+    }
+}
