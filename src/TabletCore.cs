fc5d60da71f6a6be8d78d41095b42e855822aaa9
soggylithe <nivek1892@gmail.com>
2023-04-19 12:40:29 -0500

    Auto swap stylus and book transfer recipe

--- src/TabletCore.cs
+++ src/TabletCore.cs
@@ -5,20 +5,117 @@ using System.Text;
 using System.Threading.Tasks;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
+using Vintagestory.API.Server;
 using Vintagestory.GameContent;
 
 namespace Soggylithe_Tablet
 {
     public class ModSystemTablets: ModSystem
     {
+        private bool _registered;
+        private ICoreClientAPI _capi;
+
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
 
             api.Logger.Notification("Loaded tablets.");
+        }
 
-            //TODO: register hotkey to swap offand with neck slot
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            base.StartClientSide(api);
+            _capi = api; 
+            _capi.Event.AfterActiveSlotChanged += CheckForWritable;
+            //TODO check for interacting with bookshelf - TryTake hook, inventory hook, event hook?
+        }
+
+        public void CheckHotbar(int slot)
+        {
+            //Active slot was modified
+            if (slot == _capi.World.Player.InventoryManager.ActiveHotbarSlotNumber)
+            {
+                ItemSlot active = _capi.World.Player.InventoryManager.ActiveHotbarSlot;
+                ItemSlot offhand = _capi.World.Player.Entity.LeftHandItemSlot;
+
+                if(active.Empty && offhand.Empty) { return; }
+
+                if(active.Empty && !offhand.Empty)
+                {
+                    if (offhand.Itemstack.Collectible.Attributes.IsTrue("writingTool"))
+                        PutAwayWritingTool();
+                    return;
+                }
+
+                if(!active.Empty && offhand.Empty)
+                {
+                    if(active.Itemstack.Collectible.Attributes.IsTrue("editable"))
+                        TakeOutWritingTool();
+                    return;
+                }
+
+            }
+        }
+
+        public void CheckForWritable(ActiveSlotChangeEventArgs args)
+        {
+            if (!_registered) { _capi.World.Player.InventoryManager.GetHotbarInventory().SlotModified += CheckHotbar; _registered = true; }
+
+            ItemStack fromItem = _capi.World.Player.InventoryManager.GetHotbarItemstack(args.FromSlot);
+            ItemStack toItem = _capi.World.Player.InventoryManager.GetHotbarItemstack(args.ToSlot);
+
+            bool wasEditing = fromItem != null ? fromItem.Collectible.Attributes.IsTrue("editable") : false;
+            bool willBeEditing = toItem != null ? toItem.Collectible.Attributes.IsTrue("editable") : false;
+
+            if (wasEditing && willBeEditing)
+                return;
+
+            if (willBeEditing)
+            {
+                TakeOutWritingTool(); 
+            }
+
+            if(wasEditing)
+            {
+                PutAwayWritingTool();
+            }
+        }
+
+        private void TakeOutWritingTool()
+        {
+            ItemSlot hat;
+            ItemSlot offhand;
+            offhand = _capi.World.Player.Entity.LeftHandItemSlot;
+            hat = _capi.World.Player.InventoryManager.GetOwnInventory(GlobalConstants.characterInvClassName)[(int)EnumCharacterDressType.Neck];
+
+
+            if (!offhand.Empty && offhand.Itemstack.Collectible.Attributes.IsTrue("writingTool"))
+                return;
+
+            if (!hat.Empty && hat.Itemstack.Collectible.Attributes.IsTrue("writingTool"))
+                offhand.TryFlipWith(hat);
+        }
+
+        private void PutAwayWritingTool()
+        {
+            ItemSlot hat;
+            ItemSlot offhand;
+            offhand = _capi.World.Player.Entity.LeftHandItemSlot;
+            hat = _capi.World.Player.InventoryManager.GetOwnInventory(GlobalConstants.characterInvClassName)[(int)EnumCharacterDressType.Neck];
+
+            if (!offhand.Empty && offhand.Itemstack.Collectible.Attributes.IsTrue("writingTool"))
+                offhand.TryFlipWith(hat);
+        }
+
+        public override void Dispose()
+        {
+            _capi.World.Player.InventoryManager.GetHotbarInventory().SlotModified -= CheckHotbar;
+            _capi.Event.AfterActiveSlotChanged -= CheckForWritable;
+            _registered = false;
+
+            base.Dispose();
         }
     }
 }
